bdb4463895d840a7843404db2c17e5506fde7d5c
homycdev <abdulkhamid.uzbek@gmail.com>
2019-03-17 20:06:15 +0300

    Nav bar beta

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue';
 import App from '../vue/App.vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Order from '../vue/components/Order.vue';
 import VueResource from 'vue-resource';
+
+
 Vue.use(VueResource);
+Vue.component('order-page', Order);
+
 
 new Vue({
   el: '#app',
+  
   render: h => h(App)
 });
---
+++ src/vue/components/Order.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  </div>
+</template>
+<style>
+body {
+  background-color: #fff;
+  font-family: "SergoUI";
+  color: #39cc6e;
+}
+</style>
